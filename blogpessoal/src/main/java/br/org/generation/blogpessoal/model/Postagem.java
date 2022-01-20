3f25d191e6aa67a2698d3bf55f6ed3694f135809
Brenda <contatobrendapaiva@outlook.com>
2022-01-19 22:05:06 -0300

    Blog Pessoal

---
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,66 @@
+package br.org.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name="tb_postagens")
+public class Postagem {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message="Sangue de Jesus tem Poder")
+	@Size(min=5,max=100,message="O atributo título deve conter no min a minha paciencia em 5 e no max 100")
+	private String titulo;
+	
+	@NotBlank(message="Sangue de Jesus tem Poder")
+	@Size(min=10,max=200,message="O atributo título deve conter no min a minha paciencia em 10 e no max 200")
+	private String texto;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+	
+}
