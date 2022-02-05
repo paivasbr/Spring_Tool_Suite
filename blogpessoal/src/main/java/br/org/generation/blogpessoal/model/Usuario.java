9809f417f0535771b5f64030340bf7a42a065523
Brenda <contatobrendapaiva@outlook.com>
2022-02-04 21:29:39 -0300

    Blog Pessoal Atualizações

--- blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Usuario.java
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Usuario.java
@@ -16,6 +16,8 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table(name="tb_usuarios")
 public class Usuario {
@@ -29,6 +31,7 @@ public class Usuario {
 	
 	@NotNull(message = "O atributo USUÁRIO é obrigatório!")
 	@Email(message = "O atributo USUÁRIO deve ser um e-mail válido!")
+	@Schema(example = "email@email.com.br")
 	private String usuario;
 
 	@NotBlank(message = "O atributo SENHA é obrigatório!")
